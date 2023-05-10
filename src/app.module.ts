6d69f594b8c172d45be8f6ae9b05361a9ac9456a
tech-zjf <907572438@qq.com>
2023-05-10 11:11:47 +0800

    feat:环境

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,14 @@ import { Module } from '@nestjs/common';
 import { TestModule } from './test/test.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import dbConfig from 'database/db.config';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(dbConfig), TestModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }), // 全局环境变量
+    TypeOrmModule.forRoot(dbConfig), //数据库
+    TestModule,
+  ],
   controllers: [],
   providers: [],
 })
